a681c95c5e1287aa1a212a68cf0469a6f4c9beaf
vladlevin790 <vladlevin790@gmail.com>
2026-07-11 22:35:32 +0300

    test(plugin): add Maven invoker fixtures

---
+++ cppbridge-maven-plugin/src/it/success-compile/src/main/cpp/success.cpp
@@ -0,0 +1,9 @@
+#ifdef _WIN32
+#define CPPBRIDGE_EXPORT extern "C" __declspec(dllexport)
+#else
+#define CPPBRIDGE_EXPORT extern "C"
+#endif
+
+CPPBRIDGE_EXPORT int success_value() {
+    return 42;
+}
